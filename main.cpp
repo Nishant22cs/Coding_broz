c388ec84d0e25087ce9e19cd972630510510551a
Nishant22cs <120238976+Nishant22cs@users.noreply.github.com>
2023-05-03 21:24:00 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+void create_note() {
+    string note;
+    cout << "Enter your note:" << endl;//note enter
+    getline(cin, note);
+
+    ofstream fout("notes.txt", ios::app); // open file in append mode
+    fout << note << endl; // write note to file
+    fout.close(); // close file
+
+    cout << "Note created successfully!" << endl;
+}
